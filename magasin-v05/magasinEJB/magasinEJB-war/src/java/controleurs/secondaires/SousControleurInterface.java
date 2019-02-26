f1b5b701cdbd6671f15ba2610c0b536c91bdd7de
cdi104 <cdi104@pltz853.AFPA-PARIS-DI>
2019-02-26 17:21:26 +0100

    Revert ""
    This reverts commit 7de0f6a9773258e5cccac3b51c1f7f39c7b07ca8.

--- magasin-v05/magasinEJB/magasinEJB-war/src/java/controleurs/secondaires/SousControleurInterface.java
+++
@@ -1,9 +0,0 @@
-package controleurs.secondaires;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-public interface SousControleurInterface {
-    public String process(HttpServletRequest request, HttpServletResponse response);
-}
